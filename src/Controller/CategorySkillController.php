5197f71a0c45859281495a27aef80cafb995115a
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-08 13:12:47 +0200

    add route for category

--- src/Controller/CategorySkillController.php
+++ src/Controller/CategorySkillController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\CategorySkill;
+use App\Repository\CategorySkillRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
+#[Route('api/category')]
 final class CategorySkillController extends AbstractController
 {
-    #[Route('/category/skill', name: 'app_category_skill')]
-    public function index(): JsonResponse
+    #[Route('', name: 'app_category' , methods:['GET'])]
+    public function getSkillList(CategorySkillRepository $categorySkillRepository, SerializerInterface $serializerInterface): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CategorySkillController.php',
-        ]);
+        $categorySkillList = $categorySkillRepository->findAll();
+        $jsoncategorySkillList = $serializerInterface->serialize($categorySkillList, 'json', ['groups' => "getSkills"]);
+
+        return new JsonResponse($jsoncategorySkillList, Response::HTTP_OK, [], true);
+    }
+    #[Route('/{id}', name: 'app_category_detail' , methods:['GET'])]
+    public function getSkillSlug(CategorySkill $categorySkill, SerializerInterface $serializerInterface): JsonResponse
+    {
+            $jsonSkill = $serializerInterface->serialize($categorySkill, 'json', ['groups' => "getSkills"]);
+            return new JsonResponse($jsonSkill, Response::HTTP_OK, [] , true);
     }
 }
