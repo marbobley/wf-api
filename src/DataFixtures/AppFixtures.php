1fba6e215e22932b06db7290b51de46fe2bdb938
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-08 16:35:27 +0200

    loading 2 users with fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,11 +6,35 @@ use App\Entity\CategorySkill;
 use App\Entity\Skill;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
+
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasherInterface)
+    {
+        $this->userPasswordHasher = $userPasswordHasherInterface;
+    }
+
     public function load(ObjectManager $manager): void
     {
+        //Creation user 
+        $user = new User();
+        $user->setEmail('user@skill.com');
+        $user->setRoles(["ROLE_USER"]);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, "password"));
+        $manager->persist($user);
+
+        // Création d'un user admin
+        $userAdmin = new User();
+        $userAdmin->setEmail("admin@skill.com");
+        $userAdmin->setRoles(["ROLE_ADMIN"]);
+        $userAdmin->setPassword($this->userPasswordHasher->hashPassword($userAdmin, "password"));
+        $manager->persist($userAdmin);
+
         $cat1 = new CategorySkill();
         $cat1->setName('Architecture');
         $cat1->setDescription('MVC , MVVM ....');
