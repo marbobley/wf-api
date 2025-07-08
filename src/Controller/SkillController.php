0c38aa528b5a05762cdc6a96e291706e2350f6e9
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-08 11:13:25 +0200

    add direct bind

--- src/Controller/SkillController.php
+++ src/Controller/SkillController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Skill;
 use App\Repository\SkillRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -20,14 +21,9 @@ final class SkillController extends AbstractController
         return new JsonResponse($jsonSkillList, Response::HTTP_OK, [], true);
     }
     #[Route('/api/skills/{id}', name: 'app_skill_detail' , methods:['GET'])]
-    public function getSkill(int $id, SkillRepository $skillRepository, SerializerInterface $serializerInterface): JsonResponse
+    public function getSkillSlug(Skill $skill, SerializerInterface $serializerInterface): JsonResponse
     {
-        $skill = $skillRepository->find($id);
-        if($skill)
-        {
             $jsonSkill = $serializerInterface->serialize($skill, 'json');
             return new JsonResponse($jsonSkill, Response::HTTP_OK, [] , true);
-        }
-        return new JsonResponse(null, Response::HTTP_NOT_FOUND, );
     }
 }
