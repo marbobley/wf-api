e94a8427b23a231e0c5c7d0dd0c2140093434b37
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-08 10:53:13 +0200

    loading fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Skill;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,6 +12,18 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        for($i = 0 ; $i < 20 ; $i++ ){
+            $skill = new Skill;
+            $skill->setCategory('cat_' . $i);
+            $skill->setDescription('des_' . $i);
+            $skill->setEvaluation('eva_' . $i);
+            $skill->setImgSource('imgSrc_' . $i);
+            $skill->setLanguage('lang_' . $i);
+            $skill->setLevel($i);
+            $skill->setYearOfExperience('cat_' . $i);
+
+            $manager->persist($skill);
+        }
 
         $manager->flush();
     }
