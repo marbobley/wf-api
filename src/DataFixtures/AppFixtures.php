2870bb6cf60a74f62514a3e2826118cda8aa42d8
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-09 15:39:23 +0200

    add basic category skill features

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -39,11 +39,49 @@ class AppFixtures extends Fixture
         $cat1->setName('Architecture');
         $cat1->setDescription('MVC , MVVM ....');
         $cat2 = new CategorySkill();
-        $cat2->setName('Langage');
+        $cat2->setName('Outil');
         $cat2->setDescription('');
+        $cat3 = new CategorySkill();
+        $cat3->setName('Langage');
+        $cat3->setDescription('');
+        $cat4 = new CategorySkill();
+        $cat4->setName('Framework');
+        $cat4->setDescription('');
+        $cat5 = new CategorySkill();
+        $cat5->setName('Projet');
+        $cat5->setDescription('');
+        $cat6 = new CategorySkill();
+        $cat6->setName('Architecture');
+        $cat6->setDescription('');
+        $cat7 = new CategorySkill();
+        $cat7->setName('Logiciel');
+        $cat7->setDescription('');
+        $cat8 = new CategorySkill();
+        $cat8->setName('Environnement');
+        $cat8->setDescription('');
+        $cat9 = new CategorySkill();
+        $cat9->setName('Documentation');
+        $cat9->setDescription('');
+        $cat10 = new CategorySkill();
+        $cat10->setName('Metier');
+        $cat10->setDescription('');
+        $cat11 = new CategorySkill();
+        $cat11->setName('Parler');
+        $cat11->setDescription('');
+
+
         
             $manager->persist($cat1);
             $manager->persist($cat2);
+            $manager->persist($cat3);
+            $manager->persist($cat4);
+            $manager->persist($cat5);
+            $manager->persist($cat6);
+            $manager->persist($cat7);
+            $manager->persist($cat8);
+            $manager->persist($cat9);
+            $manager->persist($cat10);
+            $manager->persist($cat11);
 
         // $product = new Product();
         // $manager->persist($product);
