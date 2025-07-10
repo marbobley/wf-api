57bcd51d7dffe9fc77dbb91f997ed79d6d448d1e
Wilfried FOURNIER <wilfried.fournier@soprasteria.com>
2025-07-10 10:06:41 +0200

    add swagger

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,7 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
