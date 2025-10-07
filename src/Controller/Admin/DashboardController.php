3a34760a5a0801f3a9012eedfa557f22294cad00
Marbobley <fournier.wilf@gmail.com>
2025-10-07 20:41:05 +0200

    add menus

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,11 +6,16 @@ use App\Entity\CategorySkill;
 use App\Entity\Skill;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
@@ -33,6 +38,27 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Skill', 'fa fa-question-circle', Skill::class);
         yield MenuItem::linkToCrud('Category', 'fa fa-question-circle', CategorySkill::class);
         yield MenuItem::linkToCrud('User', 'fa fa-users', User::class);
+        yield MenuItem::linkToUrl('Homepage', 'fas fa-home', $this->generateUrl('app_homepage'));
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
+    public function configureActions(): Actions
+    {
+        return parent::configureActions()
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        if (!$user instanceof User) {
+            throw new \Exception('Wrong user');
+        }
+
+        return parent::configureUserMenu($user)
+            ->setAvatarUrl($user->getAvatarUrl())
+            ->addMenuItems([
+                MenuItem::linkToUrl('My Profile', 'fas fa-user', $this->generateUrl('app_profil'))
+            ]);
+    }
+
+
 }
